e65d9129bec453a6da6ada50fdac51a915fcaa5a
Martin Cruz Medinilla <martin.cruz@redsinergia.com>
2020-05-02 21:21:51 -0500

    client table add

---
+++ database/database.sql
@@ -0,0 +1,11 @@
+--name database: mycompany
+CREATE TABLE PUBLIC.client(
+    client_id SERIAL NOT NULL,
+    name VARCHAR(50),
+    last_name VARCHAR(100),
+    cell VARCHAR(10),
+    observations VARCHAR(300),
+    created_date TIMESTAMP DEFAULT NOW(),
+    created_by TIMESTAMP DEFAULT NOW(),
+    update_date TIMESTAMP DEFAULT NOW()
+);
