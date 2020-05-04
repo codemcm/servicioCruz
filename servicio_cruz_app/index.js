ae606f474c83a0cdff5ea029ae37804a2d5ef5b8
Martin Cruz Medinilla <martin.cruz@redsinergia.com>
2020-05-03 22:47:47 -0500

    Base for the project

--- servicio_cruz_app/index.js
+++ servicio_cruz_app/index.js
@@ -1,11 +1,12 @@
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import NavigationApp from './Navigation';
-import TapHeaderHome from './src/components/TabHeaderHome';
 //ok
-
 import HomeDetail from './src/components/HomeWithDet';//ok for test
 import HomeTap from './src/components/HomeTap';//ok too
+import TapHeaderHome from './src/components/TabHeaderHome';
+import LoginNoTab from './src/LoginNoTab';
+import indexHome from './src/views/indexNav';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => TapHeaderHome);
+AppRegistry.registerComponent(appName, () => indexHome);
---
+++ servicio_cruz_app/src/views/clientAdd.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Component } from "react";
+import {
+    SafeAreaView,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    Alert
+} from 'react-native';
+
+import { Button } from 'react-native-elements';
+import { Input } from 'react-native-elements';
+import { Header } from 'react-native-elements';
+import { clientAdd } from '../api_functions/client_add';
+import Icon from 'react-native-vector-icons/FontAwesome';
+class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {
+        name: '',
+        last_name: '',
+        description: '',
+        cellular: ''
+    }
+    handleClick = () => {
+        if (this.state.name == '') {
+            alert('debe llenar el nombre');
+        } else {
+            data = clientAdd(this.state.name, this.state.last_name, this.state.cellular, this.state.description);
+            console.log("name:" + this.state.name);
+            this.setState({
+                name: '',
+                last_name: '',
+                description: '',
+                cellular: ''
+            })
+            alert('Datos enviados:' + data);
+        }
+
+
+    }
+    render(props) {
+        return (
+            <>
+                <StatusBar barStyle="dark-content" />
+                <SafeAreaView>
+                    <ScrollView
+                        contentInsetAdjustmentBehavior="automatic"
+                    >
+                        <View >
+                            <Text >Nombre</Text>
+                            <Input
+                                onChangeText={(text) => this.setState({ name: text })}
+                                placeholder='nombre'
+                                required={true}
+                            />
+                            <Text >Apellidos</Text>
+                            <Input
+                                placeholder='apellidos'
+                                onChangeText={(text) => this.setState({ last_name: text })}
+                            />
+                            <Text >Celular</Text>
+                            <Input
+                                placeholder='celular'
+                                onChangeText={(text) => this.setState({ cellular: text })}
+                            />
+                            <Text >Notas</Text>
+                            <Input
+                                placeholder='notas'
+                                onChangeText={(text) => this.setState({ description: text })}
+                            />
+                            <Button
+                                title="Registrar cliente"
+                                onPress={this.handleClick}
+                            />
+                        </View>
+                    </ScrollView>
+                </SafeAreaView>
+            </>
+        );
+    }
+
+};
+
+
+
+export default Home;
