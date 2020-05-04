4702e2deb48199b9f4b5a4e520a64ba3659b0e2f
Martin Cruz Medinilla <martin.cruz@redsinergia.com>
2020-05-04 06:32:14 -0500

    tabDesapear index

--- servicio_cruz_app/index.js
+++ servicio_cruz_app/index.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import NavigationApp from './Navigation';
-import Login from './src/index'
+import Login from './src/index'//tiene drawer ok
 //import Nav2 from './src/components/Nav2'
 //ok
 import HomeDetail from './src/components/HomeWithDet';//ok for test
@@ -12,4 +12,4 @@ import indexHome from './src/views/indexNav';//usar este!!
 import BaseNav from './src/components/BaseNavigation';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => Login);
+AppRegistry.registerComponent(appName, () => LoginNoTab);
