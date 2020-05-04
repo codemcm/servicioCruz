4f30f4a3e8565b5624cc935a3d20a114ef8ac1ce
Martin Cruz Medinilla <martin.cruz@redsinergia.com>
2020-05-04 06:18:23 -0500

    actual index

--- servicio_cruz_app/index.js
+++ servicio_cruz_app/index.js
@@ -1,7 +1,8 @@
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import NavigationApp from './Navigation';
-import Nav2 from './src/components/Nav2'
+import Login from './src/index'
+//import Nav2 from './src/components/Nav2'
 //ok
 import HomeDetail from './src/components/HomeWithDet';//ok for test
 import HomeTap from './src/components/HomeTap';//ok too
@@ -11,4 +12,4 @@ import indexHome from './src/views/indexNav';//usar este!!
 import BaseNav from './src/components/BaseNavigation';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => Nav2);
+AppRegistry.registerComponent(appName, () => Login);
