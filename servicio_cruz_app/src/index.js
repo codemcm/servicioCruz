584498fed87a95c6feb3fa37dfa7307a1b899ba6
Martin Cruz Medinilla <martin.cruz@redsinergia.com>
2020-05-04 06:17:53 -0500

    DRAWER OK!!!

--- servicio_cruz_app/src/index.js
+++ servicio_cruz_app/src/index.js
@@ -1,26 +1,67 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-function HomeScreen() {
+function SettingsScreen({ route, navigation }) {
+    const { user } = route.params;
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
+            <Text>Settings Screen</Text>
+            <Text>userParam: {JSON.stringify(user)}</Text>
+            <Button
+                title="Go to Profile"
+                onPress={() => navigation.navigate('Profile')}
+            />
         </View>
     );
 }
 
-const Stack = createStackNavigator();
-
-function App() {
+function ProfileScreen({ navigation }) {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Profile Screen</Text>
+        </View>
     );
 }
 
-export default App;
+function HomeScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+            <Button
+                title="Go to Settings"
+                onPress={() =>
+                    navigation.navigate('Root', {
+                        screen: 'Settings',
+                        params: { user: 'jane' },
+                    })
+                }
+            />
+        </View>
+    );
+}
+
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+function Root() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Profile" component={ProfileScreen} />
+            <Stack.Screen name="Settings" component={SettingsScreen} />
+        </Stack.Navigator>
+    );
+}
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Root">
+                <Drawer.Screen name="Root" component={Root} />
+                <Drawer.Screen name="Home" component={HomeScreen} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
+}
