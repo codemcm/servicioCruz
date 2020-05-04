bc62c6463d5c328f831e9eb2e7f5232d50419de2
Martin Cruz Medinilla <martin.cruz@redsinergia.com>
2020-05-04 04:50:02 -0500

    axios on Function ok

---
+++ servicio_cruz_app/src/api_functions/client_getAll.js
@@ -0,0 +1,15 @@
+import ConfigApi from '../config/config';
+const axios = require('axios');
+function client_getAll() {
+    return axios({
+        method: 'GET',
+        url: ConfigApi.url + "client/all/",
+        responseType: 'text'
+    })
+        .then(function (response) {
+            console.log(response.data)
+            return response;
+        });
+}
+
+export { client_getAll }
