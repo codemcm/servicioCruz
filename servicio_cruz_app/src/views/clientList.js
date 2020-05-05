58ff5f2370d69ea62f77c113ae70d57824beb4dc
Martin Cruz Medinilla <martin.cruz@redsinergia.com>
2020-05-05 10:11:37 -0500

    problem with list on react native elements

--- servicio_cruz_app/src/views/clientList.js
+++ servicio_cruz_app/src/views/clientList.js
@@ -13,7 +13,21 @@ import {client_getAll} from '../api_functions/client_getAll';
 import CState from '../components/ComponentState';
 import {clientFetch} from '../api_functions/clientFetch';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
+import {ListItem} from 'react-native-elements';
+const list = [
+  {
+    name: 'Amy Farha',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President',
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman',
+  },
+];
 function Item({objClient}) {
   return (
     <View style={styles.item}>
