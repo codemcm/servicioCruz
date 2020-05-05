05e0553c86d7c866341d0941d4cb45cf273f12ac
Martin Cruz Medinilla <martin.cruz@redsinergia.com>
2020-05-05 07:16:35 -0500

    icons add

--- servicio_cruz_app/src/views/LoginNav.js
+++ servicio_cruz_app/src/views/LoginNav.js
@@ -1,62 +1,82 @@
 import * as React from 'react';
-import { View, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
+import {View, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon5 from 'react-native-vector-icons/FontAwesome5';
+
+import IconMC from 'react-native-vector-icons/MaterialCommunityIcons';
+import IconAnt from 'react-native-vector-icons/AntDesign';
+
 import ClientAdd from './clientAdd';
 import ClientList from './clientList';
-function FeedScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Button
-                title="Go to Login"
-                onPress={() => navigation.navigate('Login')}
-            />
-        </View>
-    );
+function FeedScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button
+        title="Go to Login"
+        onPress={() => navigation.navigate('Login')}
+      />
+    </View>
+  );
 }
 
 function ProfileScreen() {
-    return <View />;
+  return <View />;
 }
 
 function AccountScreen() {
-    return <View />;
+  return <View />;
 }
 
-function SettingsScreen({ navigation }) {
-
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Button
-                title="Go to inicio"
-                onPress={() => navigation.navigate('Inicio')}
-            />
-        </View>
-    );
+function SettingsScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button
+        title="Go to inicio"
+        onPress={() => navigation.navigate('Cliente')}
+      />
+    </View>
+  );
 }
 
 const Tab = createBottomTabNavigator();
 
 function HomeTabs() {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen name="Cliente" component={ClientAdd} />
-            <Tab.Screen name="Ver" component={ClientList} />
-            <Tab.Screen name="Config" component={AccountScreen} />
-        </Tab.Navigator>
-    );
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          if (route.name === 'Cliente') {
+            return <Icon5 name="user-edit" size={20} color="#45F38B" />;
+          } else if (route.name === 'Ver') {
+            return <IconMC name="account-search" size={25} color="#45F38B" />;
+          } else if (route.name === 'Salir') {
+            return <IconAnt name="logout" size={25} color="#45F38B" />;
+          }
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: '#45F38B',
+        inactiveTintColor: 'gray',
+      }}>
+      <Tab.Screen name="Cliente" component={ClientAdd} />
+      <Tab.Screen name="Ver" component={ClientList} />
+      <Tab.Screen name="Salir" component={AccountScreen} />
+    </Tab.Navigator>
+  );
 }
 
 const Stack = createStackNavigator();
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Login" component={SettingsScreen} />
-                <Stack.Screen name="Inicio" component={HomeTabs} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={SettingsScreen} />
+        <Stack.Screen name="Cliente" component={HomeTabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- servicio_cruz_app/src/views/clientAdd.js
+++ servicio_cruz_app/src/views/clientAdd.js
@@ -1,89 +1,109 @@
 import React from 'react';
-import { Component } from "react";
+import {Component} from 'react';
 import {
-    SafeAreaView,
-    ScrollView,
-    View,
-    Text,
-    StatusBar,
-    Alert
+  SafeAreaView,
+  ScrollView,
+  View,
+  StatusBar,
+  StyleSheet,
 } from 'react-native';
 
-import { Button } from 'react-native-elements';
-import { Input } from 'react-native-elements';
-import { Header } from 'react-native-elements';
-import { clientAdd } from '../api_functions/client_add';
+import {Button} from 'react-native-elements';
+import {Input} from 'react-native-elements';
+import {Header} from 'react-native-elements';
+import {clientAdd} from '../api_functions/client_add';
+import {Text} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 class Home extends Component {
-    constructor(props) {
-        super(props);
-    }
-    state = {
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    name: '',
+    last_name: '',
+    description: '',
+    cellular: '',
+  };
+  handleClick = () => {
+    if (this.state.name == '') {
+      alert('debe llenar el nombre');
+    } else {
+      data = clientAdd(
+        this.state.name,
+        this.state.last_name,
+        this.state.cellular,
+        this.state.description,
+      );
+      console.log('name:' + this.state.name);
+      this.setState({
         name: '',
         last_name: '',
         description: '',
-        cellular: ''
+        cellular: '',
+      });
+      alert('Datos enviados:' + data);
     }
-    handleClick = () => {
-        if (this.state.name == '') {
-            alert('debe llenar el nombre');
-        } else {
-            data = clientAdd(this.state.name, this.state.last_name, this.state.cellular, this.state.description);
-            console.log("name:" + this.state.name);
-            this.setState({
-                name: '',
-                last_name: '',
-                description: '',
-                cellular: ''
-            })
-            alert('Datos enviados:' + data);
-        }
-
-
-    }
-    render(props) {
-        return (
-            <>
-                <StatusBar barStyle="dark-content" />
-                <SafeAreaView>
-                    <ScrollView
-                        contentInsetAdjustmentBehavior="automatic"
-                    >
-                        <View >
-                            <Text >Nombre</Text>
-                            <Input
-                                onChangeText={(text) => this.setState({ name: text })}
-                                placeholder='nombre'
-                                required={true}
-                            />
-                            <Text >Apellidos</Text>
-                            <Input
-                                placeholder='apellidos'
-                                onChangeText={(text) => this.setState({ last_name: text })}
-                            />
-                            <Text >Celular</Text>
-                            <Input
-                                placeholder='celular'
-                                onChangeText={(text) => this.setState({ cellular: text })}
-                            />
-                            <Text >Notas</Text>
-                            <Input
-                                placeholder='notas'
-                                onChangeText={(text) => this.setState({ description: text })}
-                            />
-                            <Button
-                                title="Registrar cliente"
-                                onPress={this.handleClick}
-                            />
-                        </View>
-                    </ScrollView>
-                </SafeAreaView>
-            </>
-        );
-    }
-
-};
-
-
+  };
+  render(props) {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView contentInsetAdjustmentBehavior="automatic">
+            <View>
+              <Text style={styles.marginText}>Nombre</Text>
+              <Input
+                placeholder="Comment"
+                leftIcon={{type: 'font-awesome', name: 'pencil'}}
+                style={styles}
+                onChangeText={text => this.setState({name: text})}
+              />
+              <Text style={styles.marginText}>Apellidos</Text>
+              <Input
+                placeholder="apellidos"
+                onChangeText={text => this.setState({last_name: text})}
+              />
+              <Text style={styles.marginText}>Celular</Text>
+              <Input
+                placeholder="celular"
+                onChangeText={text => this.setState({cellular: text})}
+              />
+              <Text style={styles.marginText}>Notas</Text>
+              <Input
+                placeholder="notas"
+                onChangeText={text => this.setState({description: text})}
+              />
+              <Button
+                onPress={this.handleClick}
+                buttonStyle={{margin: 10}}
+                icon={<Icon name="send-o" size={15} color="white" />}
+                title="Registrar"
+              />
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
 
 export default Home;
+const styles = StyleSheet.create({
+  marginButon: {
+    margin: 10,
+  },
+  marginText: {
+    marginLeft: 10,
+  },
+  container: {
+    marginTop: 50,
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
