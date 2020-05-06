5a7c185a03f7f4956b7553a2e31301b59dac9add
Martin Cruz Medinilla <martin.cruz@redsinergia.com>
2020-05-06 18:47:12 -0500

    options for menu

--- servicio_cruz_app/src/views/menu.js
+++ servicio_cruz_app/src/views/menu.js
@@ -14,25 +14,103 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Menu({navigation}) {
   return (
-    <ScrollView style={{backgroundColor: 'white'}}>
-      <View>
-        <Button
-          title="Registrar cliente"
-          onPress={() => console.log('presiono el boton...')}
-        />
-        <Button
-          title="rifa"
-          onPress={() => console.log('presiono el boton...')}
-        />
-        <Button
-          title="Productos"
-          onPress={() => console.log('presiono el boton...')}
-        />
-        <Button
-          title="Sorteo"
-          onPress={() => console.log('presiono el boton...')}
-        />
+    <ScrollView style={styles.viewDetails}>
+      <View style={styles.container}>
+        <View style={styles.containerLeft}>
+          <Button
+            title="Cliente"
+            icon={
+              <Icon
+                name="address-book"
+                size={25}
+                color="white"
+                style={{marginRight: 10}}
+              />
+            }
+            buttonStyle={styles.buttons1}
+          />
+          <Button
+            title="Rifa"
+            icon={
+              <Icon
+                name="wpforms"
+                size={15}
+                color="white"
+                style={{marginRight: 10}}
+              />
+            }
+            buttonStyle={styles.buttons2}
+          />
+          <Button
+            icon={
+              <Icon
+                name="diamond"
+                size={38}
+                color="white"
+                style={{marginRight: 10}}
+              />
+            }
+            buttonStyle={styles.buttons1}
+          />
+        </View>
+        <View style={styles.containerRight}>
+          <Button
+            title="Regalos"
+            icon={
+              <Icon
+                name="wpforms"
+                size={15}
+                color="white"
+                style={{marginRight: 10}}
+              />
+            }
+            buttonStyle={styles.buttons2}
+          />
+          <Button
+            title="Sorteo"
+            icon={
+              <Icon
+                name="wpforms"
+                size={30}
+                color="white"
+                style={{marginRight: 10}}
+              />
+            }
+            buttonStyle={styles.buttons1}
+          />
+        </View>
       </View>
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  viewDetails: {
+    backgroundColor: 'white',
+  },
+  container: {
+    flexDirection: 'row',
+  },
+  containerLeft: {
+    flex: 1,
+    marginLeft: 2,
+    marginTop: 10,
+  },
+  containerRight: {
+    flex: 1,
+    marginLeft: 2,
+    marginRight: 2,
+    marginTop: 10,
+  },
+  buttons1: {
+    marginTop: 2,
+    padding: 52,
+    backgroundColor: '#8A9075',
+  },
+  buttons2: {
+    marginTop: 2,
+    padding: 35,
+    marginBottom: 4,
+    backgroundColor: '#8AF2A2',
+  },
+});
